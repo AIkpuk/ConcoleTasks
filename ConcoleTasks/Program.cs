8d1c7ef96528bf4bf7d2c27334cd7eea56530ce9
БАБС <@DESKTOP-M1RQ99R>
2023-12-06 14:07:39 +0300

    Добавьте файлы проекта.

---
+++ ConcoleTasks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using 
+
+
+namespace ConcoleTasks
+{
+     void Foo(object a)
+    {
+        Console.WriteLine("object");
+    }
+     void Foo(object a, object b)
+    {
+        Console.WriteLine("object, object");
+    }
+     void Foo(params object[] args)
+    {
+        Console.WriteLine("params object");
+    }
+     void Foo<T>(params T[] args)
+    {
+        Console.WriteLine("params t[]");
+    }
+    class Bar { }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Foo();
+            Foo(null);
+            Foo(new Bar());
+            Foo(new Bar(), new Bar());
+            Foo(new Bar(), new object());
+        }
+
+      
+  
+        
+    }
+}
